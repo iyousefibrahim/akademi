9ea1fe638f02d0166f8d585085a70037e2a4ed07
Yousef <yousefibrahim9@outlook.com>
2026-01-25 12:36:15 +0200

    feat(i18n, theming, fonts): setup Angular app with ngx-translate, PrimeNG, and Cairo font

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,35 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
+import { MainLightTheme } from './themes/mainlight-theme';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: MainLightTheme,
+        options: {
+          darkMode: false,
+          darkModeSelector: false || '',
+        },
+      },
+    }),
+    provideHttpClient(withFetch()),
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({
+        prefix: '/assets/i18n/',
+        suffix: '.json',
+      }),
+      fallbackLang: 'en',
+      lang: 'en',
+    }),
+  ],
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslatePipe],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('akademi');
+  private translate = inject(TranslateService);
+
+  switchLang(lang: string) {
+    this.translate.use(lang);
+  }
+
+  constructor() {
+    this.translate.addLangs(['ar', 'en']);
+    this.translate.setFallbackLang('en');
+    this.translate.use('en');
+  }
 }
